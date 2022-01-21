7d1c84cb682ccbc9cad6bebd58fc67c4f13de003
02cellu02 <celestinejoy02@gmail.com>
2022-01-21 19:03:59 +0530

    Started implementing guess the game (javaScript)

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// document.querySelector('.message').textContent = '👏 Correct Number!';
+let unknownNo = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.check').addEventListener('click', function () {
+  let input = Number(document.querySelector('.guess').value);
+  //   console.log(typeof input);
+  if (!input) {
+    document.querySelector('.message').textContent =
+      "you haven't entered a number!👀";
+    return;
+  } else if (input < 1 || input > 20) {
+    document.querySelector('.message').textContent =
+      'Enter a number between 1 and 20!🙂';
+    return;
+  }
+  //   console.log(input);
+
+  if (input === unknownNo) {
+    document.querySelector('.message').textContent = '👏 Correct Number!';
+    document.querySelector('.number').textContent = unknownNo;
+    let highScore = document.querySelector('.highscore').textContent;
+    let currentscore = document.querySelector('.score').textContent;
+    if (highScore < currentscore)
+      document.querySelector('.highscore').textContent = currentscore;
+  } else {
+    document.querySelector('.score').textContent -= 1;
+    if (input < unknownNo) {
+      document.querySelector('.message').textContent = 'Too Low 😢';
+    } else {
+      document.querySelector('.message').textContent = 'Too high 😔';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.guess').value = null;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  unknownNo = Math.trunc(Math.random() * 20) + 1;
+});
